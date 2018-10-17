68e816a87b4c5c13451acef9ea99a3dd92b15653
github username <github email address>
2018-10-17 13:47:11 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
+# require 'pry'
+
 class Person
-  #your code here
+attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+:weight, :handed, :complexion, :t_shirt_size,
+:wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+  attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
 end
+
+# sarah_attributes = {name: "Sarah", hair_color: "Black"}
+#
+# sarah = Person.new(sarah_attributes)
+# sarah.name
+# sarah.hair_color
+
+# binding.pry
+
+# 0
